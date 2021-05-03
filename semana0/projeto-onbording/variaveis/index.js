92755dc6d99580a99e657a31c1eb4d27ae0ac1a6
millagmc <milla.revolts@gmail.com>
2021-05-03 12:54:09 -0300

    Codigos da primeira aula de javascript

---
+++ semana0/projeto-onbording/variaveis/index.js
@@ -0,0 +1,10 @@
+//console.log ("Hello World")
+//console.log("aaaaaaaaaaaaa")
+//console.log("vai se fuder codigo chato")
+//const nome = prompt ("what is your name?")
+
+//console.log(nome)
+
+const nome = prompt ("What is your name?")
+const age = prompt ("What is your age?")
+console.log("o tipo de variavel para nome sera" ,typeof nome, "e da idade sera", typeof age)
---
+++ semana1/variaveis/index.js
@@ -0,0 +1,29 @@
+/*const nomeDoUsuario = prompt ("Qual o seu nome?")
+let idade = Number (prompt ("Qual a sua idade?"))
+console.log ("o tipo para nome sera", typeof nomeDoUsuario)
+console.log("o tipo para idade sera", typeof idade)
+
+//E. o primeiro sera string pois todos retornam string o segundo sera numero 
+//pq eu o convertir de string para numero
+
+//F. sem o number as duas voltaram string
+
+console.log ("Olá ",nomeDoUsuario ," você tem ",idade," anos")
+*/
+
+/* const roupa = prompt ("Voce esta usando uma blusa rosa hoje?")
+const comida = prompt ("Voce comeu banana hoje?")
+const unhas = prompt ("Voce pintou suas unhas essas semana?")
+    const resposta1 = roupa
+    const resposta2 = comida
+    const resposta3 = unhas
+        console.log ("Voce esta usando uma blusa rosa hoje?", resposta1)
+        console.log ("Voce comeu banana hoje?", resposta2)
+        console.log("Voce pintou suas unhas essas semana?", resposta3) */
+
+/*
+let valorDeA = prompt ("Digite o valor de A")
+let valorDeB = prompt ("Digite o valor de B")
+    valorFinalA=valorDeB
+    valorFinalB=valorDeA
+        console.log("Valores finais serao: A=", valorFinalA ,"B=", valorFinalB)*/
