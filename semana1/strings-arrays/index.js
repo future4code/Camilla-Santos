9181d46a08a020266e9e9ec1c9d1aa8bc51d017b
millagmc <milla.revolts@gmail.com>
2021-05-05 15:59:46 -0300

    Strings e Arrays exercicios e desafios

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,54 @@
+//      Questao 1 de interpretar codigo
+//no a o array vai estar indefinido, no b estara null 
+// no c sera igual a 11 ja que este sera o tamanho do array
+//no d mostrara o 3 ja que ele esta na posicao 0  
+//no e troca posicao do 4 pelo 19
+// o f eu nao entendi
+ 
+
+/*       Questao 2
+
+A frase ficara SUBI NUM ONIBUS EM MIRROCOS
+ TAMANHO OU LENGTH 27 
+
+        Questao 1 de codigos
+
+const nomeDoUsuario = prompt('What is your name?')
+const emailDoUsuario = prompt('What is your email address?')
+console.log(`O email ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vindo(a), ${nomeDoUsuario}`)
+        Questao 2 de codigos
+const comidasFavoritas =['acaraje', 'vatapa','pao de queijo', 'feijoada', 'caldo de sururu']
+//a
+console.log(comidasFavoritas)
+//b
+console.log('Essa sao minhas comidas preferidas:',comidasFavoritas) 
+c
+const comidasFavoritas =['acaraje', 'vatapa','pao de queijo', 'feijoada', 'caldo de sururu']
+console.log(comidasFavoritas)
+const qualSuaComida = prompt ('Qual a sua comida favorita?')
+comidasFavoritas.splice(1,1, qualSuaComida)
+console.log('A minhas comidas favoritas sao',comidasFavoritas)
+ d e f
+var listaDeTarefas = [];
+listaDeTarefas[0] = prompt('Digite  a primeira tarefa que precisa realizar')
+listaDeTarefas[1]= prompt ('Digite a segunda tarefa que que precisa realizar ')
+listaDeTarefas[2] =  prompt('Digite a terceira tarefa que precisa realizar')
+console.log(listaDeTarefas)
+const numero = prompt ('Digite o numero de 1 a 3 para indicar que tarefa voce terminou')
+listaDeTarefas.splice(numero-1,1)
+console.log (`Suas atividades restantes sao:  `, listaDeTarefas)
+
+desafio 1
+var frase = [];
+var frase  = prompt ('Digite uma frase:')
+frase.split()
+//frase = frase.replaceAll (' ','')
+console.log(frase) 
+
+
+ //desafio 2
+
+ const array = ['banana', 'morango',"Abacaxi", "Laranja", "Ameixa" ]
+ var indiceAbacaxi = array.indexOf("Abacaxi");
+ console.log('O indice sera', indiceAbacaxi,)
+ console.log(' E o tamanha sera:', indiceAbacaxi.length()) */
